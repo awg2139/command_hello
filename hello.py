3d4123d04b735b52675f7ed9eb8ed002787b5d17
awg2139 <whsbyq1@gmail.com>
2025-04-11 09:54:08 +0900

    github for git pull

--- hello.py
+++ hello.py
@@ -1,8 +1,9 @@
 // for command line hit tutorial
 // local repository modification
-//github modification
+// github modification
 
 print("hello world")
 print("tell your world")
 print("tell his world")
 print("tell her world")
+print("tell my world")
